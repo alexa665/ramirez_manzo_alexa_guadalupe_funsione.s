226d14278ede526728ebcedc6c235565dfaec51a
alexa665 <a.23308051281205@cbtis128.edu.mx>
2024-10-30 15:39:00 -0600

    Add files via upload

---
+++ funciones12.py
@@ -0,0 +1,8 @@
+print(" ")
+print("alexa guadalupe ramirez manzo 1205 3-W")
+print(" ")
+#functionLas definiciones no pueden estar vacías, pero si por alguna razón tienes una functiondefinición sin contenido, 
+# ponla en la passdeclaración para evitar obtener un error.
+
+def myfunction():
+  pass
