22834bc845dcf6e725f9c3c63523ff91fd4ae807
alexa665 <a.23308051281205@cbtis128.edu.mx>
2024-10-29 20:10:45 -0600

    Add files via upload

---
+++ funciones2.py
@@ -0,0 +1,9 @@
+print(" ")
+print("alexa guadalupe ramirez manzo 1205 3-W")
+print(" ")
+def my_function(fname):
+  print(fname + " manzo ")
+
+my_function("alexa")
+my_function("guadalupe")
+my_function("ramirez")
