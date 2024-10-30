dfe3604962c8e22e7d01edbb0f8ce0fbfe20498f
alexa665 <a.23308051281205@cbtis128.edu.mx>
2024-10-29 20:26:01 -0600

    Add files via upload

---
+++ funciones4.py
@@ -0,0 +1,9 @@
+print(" ")
+print("alexa guadalupe ramirez manzo 1205 3-W")
+print(" ")
+#Esta función espera 2 argumentos, pero solo obtiene 1:
+
+def my_function(fname):
+  print(fname)
+
+my_function("hola")
